5134954901995f2cfb3fcb8af6995c70c8672d07
Moataz Mohamed <moataz9898k@gmail.com>
2025-02-21 20:59:21 +0200

    s

---
+++ data/index.ts
@@ -0,0 +1,136 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently I am expanding my skillset into 3d development",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/code3.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Nexus Ecommerce",
+    des: "NEXUS is a leading e-commerce website that offers a vast selection of products",
+    img: "/nexus.png",
+    iconLists: ["/ng.png", "/tail.svg", "/ts.svg", "/fire.png"],
+    link: "https://nexus-ecommerce-a42d1.web.app/home",
+  },
+  {
+    id: 2,
+    title: "Vera Software Solution",
+    des: "Business website for software solutions",
+    img: "/vera.png",
+    iconLists: ["html.png", "css.png", "bots.png", "js.png"],
+    link: "https://moatazmohaamed.github.io/Vera-software-solutions/",
+  },
+  {
+    id: 3,
+    title: "3d Minecraft diamond ore",
+    des: "Minecraft diamond ore made with threejs webGl",
+    img: "/3d.png",
+    iconLists: ["html.png", "/tail.svg", "js.png", "/three.svg"],
+    link: "https://3d-diamond-ore.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Yavin office",
+    des: "Yavin is a software solution company",
+    img: "/yavin.png",
+    iconLists: ["html.png", "css.png", "bots.png", "js.png"],
+    link: "https://moatazmohaamed.github.io/Yavin-Office-Design/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+
+  {
+    id: 2,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/moatazmohaamed",
+  },
+
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/moataz-mohamed-460a83314/",
+  },
+];
